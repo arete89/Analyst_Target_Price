2eee803143b29241125bd8250b34237b245a2639
Ronald Kim <75988771+arete89@users.noreply.github.com>
2021-10-03 22:58:36 +0900

    Update yFinance_Target.py

--- yFinance_Target.py
+++ yFinance_Target.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 
 tickers = ['AMT', 'LNG', 'MRK', 'LEVI', 'RIO']
+#ticker can be any stock in your portfolio, or whatever you've been following lately
 targets = []
 
 for item in tickers:
@@ -12,20 +13,20 @@ for item in tickers:
               'corsDomain=finance.yahoo.com'
 
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'}
+        'User-Agent': ''}
+    #Add your own user agent address - Just google it on your browser.
     url = lhs_url + item + rhs_url
     r = requests.get(url, headers=headers)
     result = r.json()['quoteSummary']['result'][0]
     target = result['financialData']['targetMeanPrice']['fmt']
-
+    #If you want the median version, then replace 'targetMeanPrice' with 'targetMedianPrice'
     targets.append(target)
 
     print("--------------------------------------------")
     print("{} has an average target price of: ".format(item), target)
-    # time.sleep(0.5)
 
 dataframe = pd.DataFrame(list(zip(tickers, targets)), columns=['Company', 'Mean Target Price'])
 dataframe = dataframe.set_index('Company')
 dataframe.to_csv('Target_Price.csv')
-
+#to_excel in order to save it as a .xlsx file
 print(dataframe)
